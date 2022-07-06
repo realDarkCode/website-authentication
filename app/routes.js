cfb3872e98eff314fb82c6a42a6f4df19fdf302f
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-07-06 12:58:02 +0600

    refactor[routes]: api use

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,6 @@ const applicationRoutes = require("../routes/");
 router.get("/health", (_req, res) => {
 	res.status(200).json({ message: "success" });
 });
-router.use(applicationRoutes);
+router.use("/api/v1", applicationRoutes);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const authRoutes = require("./auth");
 
-router.use("/api/v1/auth", authRoutes);
+router.use("/auth", authRoutes);
 
 router.get("/", (_req, res) => {
 	res.status(200).json({ message: "Welcome to the API" });
