2c9b240bf33bbb39ef23ed6cdc8cdbd7a0cb1ce5
Tanzil Islam <tanzil.ncpsc@gmail.com>
2022-07-06 12:52:56 +0600

    refactor: removed views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const path = require("path");
 const authRoutes = require("./auth");
-router.use(authRoutes);
+
+router.use("/api/v1/auth", authRoutes);
 
 router.get("/", (_req, res) => {
-	res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+	res.status(200).json({ message: "Welcome to the API" });
 });
 module.exports = router;
